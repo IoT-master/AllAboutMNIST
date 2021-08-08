12231f1a48953fd82308d8faf24b83a6a13c5af5
Senh Mo Chuang <senhmo@gmail.com>
2021-08-08 00:33:59 -0500

    first commit

---
+++ reading_handwritten_digits_cuda.py
@@ -0,0 +1,74 @@
+import torch
+from torchvision import transforms, datasets
+
+train = datasets.MNIST("", train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+test = datasets.MNIST("", train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+
+BATCH_SIZE = 10
+
+trainset = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
+testset = torch.utils.data.DataLoader(test, batch_size=BATCH_SIZE, shuffle=True)
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(28*28, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return F.log_softmax(x, dim=1)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("CUDA detected")
+else:
+    device = torch.device("cpu")
+    print("GPU NOT detected!")
+
+net = Net().to(device)
+print(net)
+
+import torch.optim as optim
+
+optimizer = optim.Adam(net.parameters(), lr=.001)
+
+EPOCHS = 3
+
+for epocs in range(EPOCHS):
+    for X, y in trainset:
+        net.zero_grad()
+        output = net(X.view(-1, 28*28).to(device))
+        loss = F.nll_loss(output, y.to(device))
+        loss.backward()
+        optimizer.step()
+    print(loss)
+
+correct, total = 0, 0
+with torch.no_grad():
+    for X, y in trainset:
+        output = net(X.view(-1, 28*28).to(device))
+        for idx, i in enumerate(output):
+            if torch.argmax(i) == y.to(device)[idx]:
+                correct += 1
+            total += 1
+print(f"Accuracy: {round(correct/total, 3)}")
+
+correct, total = 0, 0
+with torch.no_grad():
+    for X, y in testset:
+        output = net(X.view(-1, 28*28).to(device))
+        for idx, i in enumerate(output):
+            if torch.argmax(i) == y.to(device)[idx]:
+                correct += 1
+            total += 1
+print(f"Validation: {round(correct/total, 3)}")
