5442939b54bc757793f982252bfe02e0f576f230
Senh Mo Chuang <senhmo@gmail.com>
2021-08-15 11:18:39 -0500

    Feed Forward Now Backward

--- reading_real_handwritten_digits_cuda.py
+++ reading_real_handwritten_digits_cuda.py
@@ -16,7 +16,6 @@ testing_df = pd.read_csv(Path(MNIST_DIR).joinpath('test.csv'))
 mnist_dir = Path.cwd().joinpath(MNIST_DIR)
 
 
-
 def spatial_size(input_size: int, kernel_size: int, stride: int = 1, padding: int = 0):
     # https://cs231n.github.io/convolutional-networks/
     spatial_size = (input_size - kernel_size + 2 * padding)/stride + 1
@@ -26,20 +25,6 @@ def spatial_size(input_size: int, kernel_size: int, stride: int = 1, padding: in
         f'You will have {spatial_size**2:.0f} feature maps if square dimensions')
     return int(spatial_size)
 
-class MNISTModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(28*28, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, 10)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
-        return F.log_softmax(x, dim=1)
 
 class MNISTDataset(Dataset):
 
@@ -54,17 +39,19 @@ class MNISTDataset(Dataset):
         return len(self.training_df)
 
     def __getitem__(self, index) -> dict:
-        path_to_image = Path(self.training_path).joinpath(self.training_df['filename'].iloc[index])
+        path_to_image = Path(self.training_path).joinpath(
+            self.training_df['filename'].iloc[index])
         image = cv2.imread(str(path_to_image), cv2.IMREAD_GRAYSCALE)
         torched_image = torch.Tensor(image)
         image_x, image_y = torched_image.shape
         cv2_converted_tensor = torched_image.view(-1, image_x, image_y)
-        normalized_filter = transforms.Normalize(torch.mean(cv2_converted_tensor), torch.std(cv2_converted_tensor))
+        normalized_filter = transforms.Normalize(torch.mean(
+            cv2_converted_tensor), torch.std(cv2_converted_tensor))
         data = normalized_filter(cv2_converted_tensor)
         label = self.training_df['label'].iloc[index]
         sample = {
             'image': data,
-            'label': label
+            'label': torch.tensor([label])
         }
 
         if self.transform:
@@ -72,13 +59,49 @@ class MNISTDataset(Dataset):
 
         return sample
 
-training_dataset = MNISTDataset()
-batch_loader_params = {
-    "batch_size": 50,
-    "shuffle": True,
-    "num_workers": 0 if platform.system() == 'Windows' else 2
-}
-dataloader = DataLoader(training_dataset, **batch_loader_params)
+
+class MNISTModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 60)
+        self.fc2 = nn.Linear(60, 10)
+
+    def forward(self, x):
+        # [b, 1, 28, 28] ==> [b, 20, 24, 24]
+        x = F.relu(self.conv1(x))
+        # [b, 20, 24, 24] ==> [b, 20, 12, 12]
+        x = F.max_pool2d(x, 2, 2)
+        # [b, 20, 12, 12] ==> [b, 50, 8, 8]
+        x = F.relu(self.conv2(x))
+        # [b, 50, 8, 8] ==> [b, 50, 4, 4]
+        x = F.max_pool2d(x, 2, 2)
+        # [b, 50, 4, 4] ==> [b, 50*4*4]
+        x = x.view(-1, 4*4*50)
+        # [b, 50*4*4] ==> [b, 60]
+        x = F.relu(self.fc1(x))
+        # [b, 60] ==> [b, 10]
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
 
 if __name__ == "__main__":
-    print(spatial_size(28, 3))
+    train_dataset = MNISTDataset()
+
+    model = MNISTModel()
+
+    sample = next(iter(train_dataset))
+    image = sample['image']
+    labels = sample['label']
+    print(image.shape, labels.shape)
+    image_batch = image.unsqueeze(0)
+
+    preds = model(image_batch)
+    loss = F.cross_entropy(preds, labels)
+    print(loss.item())
+
+    print(model.conv1.weight.grad)
+    loss.backward()
+    print(model.conv1.weight.grad)
+    print(model.conv1.weight.grad.shape)
--- testing_read_hw_digits.py
+++ testing_read_hw_digits.py
@@ -1,5 +1,5 @@
 import unittest
-from reading_real_handwritten_digits_cuda import MNISTDataset, DataLoader
+from reading_real_handwritten_digits_cuda import MNISTDataset, DataLoader, MNISTModel, spatial_size
 import torch
 import platform
 
@@ -23,13 +23,34 @@ class TestingHWDigits(unittest.TestCase):
         dataset = MNISTDataset()
         sample = dataset.__getitem__(0)
         self.assertEqual(sample['image'].shape, torch.Size([1, 28, 28]), 'Dimensions are not correct')
-        self.assertEqual(sample['label'], 4, 'Dimensions are not correct')
+        self.assertEqual(sample['label'].shape, torch.Size([1]), 'Dimensions are not correct')
 
     def test_basic_dataloader(self):
         sample = iter(self.dataloader)
         temp = sample.next()
-        self.assertEqual(temp['label'].shape, torch.Size([50]), 'Label is broken')
+        self.assertEqual(temp['label'].shape, torch.Size([50, 1]), 'Label is broken')
         self.assertEqual(temp['image'].shape, torch.Size([50, 1, 28, 28]), 'Dataloader input is broken')
 
+    def test_spatial_size(self):
+        self.assertEqual(spatial_size(28, 5), 24, 'Spatial Size function is broken')
+
+    def test_one_sample_into_model(self):
+            training_dataset = MNISTDataset()
+            batch_loader_params = {
+                "batch_size": 50,
+                "shuffle": True,
+                "num_workers": 0 if platform.system() == 'Windows' else 2
+            }
+            dataloader = DataLoader(training_dataset, **batch_loader_params)
+            model = MNISTModel()
+            sample = next(iter(training_dataset))
+            image = sample['image']
+            label = sample['label']
+            image_batch = image.unsqueeze(0)
+            self.assertEqual(image_batch.shape, torch.Size([1, 1, 28, 28]), 'Your dimensions of your input should be: Batch, Channels, Pixel Width, Pixel Height')
+            self.assertEqual(model(image_batch).shape, torch.Size([1, 10]), 'Your dimensions of your input should be: Batch, Possible Outcomes')
+
+    def tearDown(self) -> None:
+        pass
 if __name__=='__main__':
     unittest.main()
